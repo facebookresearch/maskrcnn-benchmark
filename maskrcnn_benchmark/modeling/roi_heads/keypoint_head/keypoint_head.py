72425b1cdfb6409208f2e9f9c5ab291b92e85d87
Francisco Massa <fvsmassa@gmail.com>
2018-10-29 11:10:39 -0700

    Training seems to work
    Still gives slightly worse results

--- maskrcnn_benchmark/modeling/roi_heads/keypoint_head/keypoint_head.py
+++ maskrcnn_benchmark/modeling/roi_heads/keypoint_head/keypoint_head.py
@@ -3,7 +3,30 @@ import torch
 from .roi_keypoint_feature_extractors import make_roi_keypoint_feature_extractor
 from .roi_keypoint_predictors import make_roi_keypoint_predictor
 from .inference import make_roi_keypoint_post_processor
-# from .loss import make_roi_keypoint_loss_evaluator
+from .loss import make_roi_keypoint_loss_evaluator
+
+
+def keep_only_positive_boxes(boxes):
+    """
+    Given a set of BoxList containing the `labels` field,
+    return a set of BoxList for which `labels > 0`.
+
+    Arguments:
+        boxes (list of BoxList)
+    """
+    assert isinstance(boxes, (list, tuple))
+    assert isinstance(boxes[0], BoxList)
+    assert boxes[0].has_field("labels")
+    positive_boxes = []
+    positive_inds = []
+    num_boxes = 0
+    for boxes_per_image in boxes:
+        labels = boxes_per_image.get_field("labels")
+        inds_mask = labels > 0
+        inds = inds_mask.nonzero().squeeze(1)
+        positive_boxes.append(boxes_per_image[inds])
+        positive_inds.append(inds_mask)
+    return positive_boxes, positive_inds
 
 
 class ROIKeypointHead(torch.nn.Module):
@@ -13,18 +36,23 @@ class ROIKeypointHead(torch.nn.Module):
         self.feature_extractor = make_roi_keypoint_feature_extractor(cfg)
         self.predictor = make_roi_keypoint_predictor(cfg)
         self.post_processor = make_roi_keypoint_post_processor(cfg)
-        # self.loss_evaluator = make_roi_keypoint_loss_evaluator(cfg)
+        self.loss_evaluator = make_roi_keypoint_loss_evaluator(cfg)
 
     def forward(self, features, proposals, targets=None):
-        # the network can't handle the case of no selected proposals,
-        # so need to shortcut before
-        # TODO handle this properly
-        if not self.training:
-            if sum(r.bbox.shape[0] for r in proposals) == 0:
-                for r in proposals:
-                    r.add_field("keypoints", features[0].new())
-                return features, proposals, {}
+        """
+        Arguments:
+            features (list[Tensor]): feature-maps from possibly several levels
+            proposals (list[BoxList]): proposal boxes
+            targets (list[BoxList], optional): the ground-truth targets.
 
+        Returns:
+            x (Tensor): the result of the feature extractor
+            proposals (list[BoxList]): during training, the original proposals
+                are returned. During testing, the predicted boxlists are returned
+                with the `mask` field set
+            losses (dict[Tensor]): During training, returns the losses for the
+                head. During testing, returns an empty dict.
+        """
         if self.training:
             with torch.no_grad():
                 proposals = self.loss_evaluator.subsample(proposals, targets)
--- maskrcnn_benchmark/modeling/roi_heads/keypoint_head/loss.py
+++ maskrcnn_benchmark/modeling/roi_heads/keypoint_head/loss.py
@@ -2,244 +2,172 @@ import torch
 from torch.nn import functional as F
 
 from maskrcnn_benchmark.modeling.matcher import Matcher
-from maskrcnn_benchmark.modeling.losses.target_preparator import TargetPreparator
-from maskrcnn_benchmark.modeling.utils import cat, cat_bbox, split_with_sizes, keep_only_positive_boxes
+
+from maskrcnn_benchmark.modeling.balanced_positive_negative_sampler import (
+    BalancedPositiveNegativeSampler
+)
+from maskrcnn_benchmark.structures.boxlist_ops import boxlist_iou
+from maskrcnn_benchmark.modeling.utils import cat
 from maskrcnn_benchmark.layers import smooth_l1_loss
+from maskrcnn_benchmark.structures.boxlist_ops import cat_boxlist
 
 from maskrcnn_benchmark.structures.keypoint import keypoints_to_heat_map
 
 
-class KeypointTargetPreparator(TargetPreparator):
-    def __init__(self, proposal_matcher, discretization_size):
-        super(KeypointTargetPreparator, self).__init__(proposal_matcher, None)
-        self.discretization_size = discretization_size
+def project_keypoints_to_heatmap(keypoints, proposals, discretization_size):
+    proposals = proposals.convert('xyxy')
+    return keypoints_to_heat_map(keypoints.keypoints, proposals.bbox, discretization_size)
 
-    def index_target(self, target, index):
-        target = target.copy_with_fields(['labels', 'keypoints'])
-        return target[index]
 
-    def prepare_labels(self, matched_targets_per_image, anchors_per_image):
-        matched_idxs = matched_targets_per_image.get_field('matched_idxs')
-        labels_per_image = matched_targets_per_image.get_field('labels')
-        labels_per_image = labels_per_image.to(dtype=torch.int64)
+def cat_boxlist_with_keypoints(boxlists):
+    assert all(boxlist.has_field("keypoints") for boxlist in boxlists)
 
-        keypoints = matched_targets_per_image.get_field('keypoints')
-        # TODO remove conditional  when better support for zero-dim is in
-        if keypoints.keypoints.numel() > 0:
-            within_box = self._within_box(keypoints.keypoints, matched_targets_per_image.bbox)
-            vis_kp = keypoints.keypoints[..., 2] > 0
-            is_visible = (within_box & vis_kp).sum(1) > 0
+    kp = [boxlist.get_field('keypoints').keypoints for boxlist in boxlists]
+    kp = cat(kp, 0)
 
-            labels_per_image[~is_visible] = -1
+    fields = boxlists[0].get_fields()
+    fields = [field for field in fields if field != "keypoints"]
 
-        # this can probably be removed, but is left here for clarity
-        # and completeness
-        neg_inds = matched_idxs == Matcher.BETWEEN_THRESHOLDS
-        labels_per_image[neg_inds] = 0
+    boxlists = [boxlist.copy_with_fields(fields) for boxlist in boxlists]
+    boxlists = cat_boxlist(boxlists)
+    boxlists.add_field("keypoints", kp)
+    return boxlists
 
-        return labels_per_image, keypoints
 
-    def _within_box(self, points, boxes):
-        """Validate which keypoints are contained inside a given box.
-        points: NxKx2
-        boxes: Nx4
-        output: NxK
-        """
-        x_within = (points[..., 0] >= boxes[:, 0, None]) & (points[..., 0] <= boxes[:, 2, None])
-        y_within = (points[..., 1] >= boxes[:, 1, None]) & (points[..., 1] <= boxes[:, 3, None])
-        return x_within & y_within
+def _within_box(points, boxes):
+    """Validate which keypoints are contained inside a given box.
+    points: NxKx2
+    boxes: Nx4
+    output: NxK
+    """
+    x_within = (points[..., 0] >= boxes[:, 0, None]) & (points[..., 0] <= boxes[:, 2, None])
+    y_within = (points[..., 1] >= boxes[:, 1, None]) & (points[..., 1] <= boxes[:, 3, None])
+    return x_within & y_within
 
-    def project(self, keypoints, anchors):
-        """
-        """
-        M = self.discretization_size
-        device = anchors.bbox.device
-        positive_anchors = anchors.convert('xyxy')
-        # assert keypoints.size == anchors.size, '{}, {}'.format(
-        #         keypoints, anchors)
-
-        # return keypoints_to_heat_map(keypoints.keypoints, anchors.bbox, M)
-        return keypoints_to_heat_map(keypoints, anchors.bbox, M)
-
+_TOTAL_SKIPPED = 0
 
 class KeypointRCNNLossComputation(object):
-    def __init__(self, target_preparator, fg_bg_sampler):
+
+    def __init__(self, proposal_matcher, fg_bg_sampler, discretization_size):
         """
+        Arguments:
+            proposal_matcher (Matcher)
+            fg_bg_sampler (BalancedPositiveNegativeSampler)
+            discretization_size (int)
         """
-        self.target_preparator = target_preparator
+        self.proposal_matcher = proposal_matcher
         self.fg_bg_sampler = fg_bg_sampler
+        self.discretization_size = discretization_size
+
+    def match_targets_to_proposals(self, proposal, target):
+        match_quality_matrix = boxlist_iou(target, proposal)
+        matched_idxs = self.proposal_matcher(match_quality_matrix)
+        # Keypoint RCNN needs "labels" and "keypoints "fields for creating the targets
+        target = target.copy_with_fields(["labels", "keypoints"])
+        # get the targets corresponding GT for each proposal
+        # NB: need to clamp the indices because we can have a single
+        # GT in the image, and matched_idxs can be -2, which goes
+        # out of bounds
+        matched_targets = target[matched_idxs.clamp(min=0)]
+        matched_targets.add_field("matched_idxs", matched_idxs)
+        return matched_targets
+
+    def prepare_targets(self, proposals, targets):
+        labels = []
+        keypoints = []
+        for proposals_per_image, targets_per_image in zip(proposals, targets):
+            matched_targets = self.match_targets_to_proposals(
+                proposals_per_image, targets_per_image
+            )
+            matched_idxs = matched_targets.get_field("matched_idxs")
+
+            labels_per_image = matched_targets.get_field("labels")
+            labels_per_image = labels_per_image.to(dtype=torch.int64)
+
+            # this can probably be removed, but is left here for clarity
+            # and completeness
+            # TODO check if this is the right one, as BELOW_THRESHOLD
+            neg_inds = matched_idxs == Matcher.BETWEEN_THRESHOLDS
+            labels_per_image[neg_inds] = 0
 
 
-    # TODO remove as it's almost a copy-paste from fast_rcnn_losses
-    def subsample(self, anchors, targets):
+
+            keypoints_per_image = matched_targets.get_field('keypoints')
+            # TODO remove conditional  when better support for zero-dim is in
+            if keypoints_per_image.keypoints.numel() > 0:
+                within_box = _within_box(keypoints_per_image.keypoints, matched_targets.bbox)
+                vis_kp = keypoints_per_image.keypoints[..., 2] > 0
+                is_visible = (within_box & vis_kp).sum(1) > 0
+
+                labels_per_image[~is_visible] = -1
+
+            labels.append(labels_per_image)
+            keypoints.append(keypoints_per_image)
+
+        return labels, keypoints
+
+
+    def subsample(self, proposals, targets):
         """
         This method performs the positive/negative sampling, and return
-        the sampled anchors.
+        the sampled proposals.
         Note: this function keeps a state.
+
         Arguments:
-            anchors (list of list of BoxList)
-            targets (list of BoxList)
+            proposals (list[BoxList])
+            targets (list[BoxList])
         """
 
-        labels, keypoints = self.prepare_targets(anchors, targets)
+        labels, keypoints = self.prepare_targets(proposals, targets)
         sampled_pos_inds, sampled_neg_inds = self.fg_bg_sampler(labels)
         # FIXME hack to simplify a few things.
         sampled_neg_inds = [i.new() for i in sampled_neg_inds]
 
-        # flip anchors to be images -> feature map levels
-        anchors = list(zip(*anchors))
-        levels = [torch.tensor([i for i, n in enumerate(anchor)
-            for _ in range(n.bbox.shape[0])]) for anchor in anchors]
-        num_levels = len(anchors[0])
-        num_images = len(anchors)
-        # concatenate all anchors for the same image
-        anchors = [cat_bbox(anchors_per_image) for anchors_per_image in anchors]
+        proposals = list(proposals)
+        # add corresponding label and regression_targets information to the bounding boxes
+        for labels_per_image, keypoints_per_image, proposals_per_image in zip(
+            labels, keypoints, proposals
+        ):
+            proposals_per_image.add_field("labels", labels_per_image)
+            proposals_per_image.add_field(
+                "keypoints", keypoints_per_image
+            )
 
-        # add corresponding label information to the bounding boxes
-        # this can be used with `keep_only_positive_boxes` in order to
-        # restrict the set of boxes to be used during other steps (Mask R-CNN
-        # for example)
-        for labels_per_image, keypoints_per_image, anchors_per_image in zip(
-                labels, keypoints, anchors):
-            anchors_per_image.add_field('labels', labels_per_image)
-            anchors_per_image.add_field('keypoints', keypoints_per_image)
-
-        sampled_inds = []
-        sampled_image_levels = []
-        # distributed sampled anchors, that were obtained on all feature maps
+        # distributed sampled proposals, that were obtained on all feature maps
         # concatenated via the fg_bg_sampler, into individual feature map levels
         for img_idx, (pos_inds_img, neg_inds_img) in enumerate(
-                zip(sampled_pos_inds, sampled_neg_inds)):
-            img_sampled_inds = pos_inds_img  # | neg_inds_img
-            anchors_per_image = anchors[img_idx][img_sampled_inds]
-            sampled_levels = levels[img_idx][img_sampled_inds]
-            # keypoints_per_image = keypoints[img_idx][img_sampled_inds]
-            # TODO replace with bincount because indices in the same level
-            # are packed together
-            anchors_per_level_per_image = []
-            sampled_image_level_temp = []
-            # keypoints_per_level_per_image = []
-            for level in range(num_levels):
-                level_idx = nonzero(sampled_levels == level)[0]
-                anchors_per_level_per_image.append(anchors_per_image[level_idx])
-                sampled_image_level_temp.append(torch.full_like(level_idx, img_idx))
-            anchors[img_idx] = anchors_per_level_per_image
-            sampled_inds.append(img_sampled_inds)
-            sampled_image_levels.append(sampled_image_level_temp)
+            zip(sampled_pos_inds, sampled_neg_inds)
+        ):
+            # img_sampled_inds = torch.nonzero(pos_inds_img | neg_inds_img).squeeze(1)
+            img_sampled_inds = torch.nonzero(pos_inds_img).squeeze(1)
+            proposals_per_image = proposals[img_idx][img_sampled_inds]
+            proposals[img_idx] = proposals_per_image
 
-        # flip back to original format feature map level -> images
-        anchors = list(zip(*anchors))
+        self._proposals = proposals
+        return proposals
 
-        labels = torch.cat(labels, dim=0)
-
-        # find permutation that brings the concatenated representation in the order
-        # that first joins the images for the same level, and then concatenates the
-        # levels into the representation obtained by concatenating first the feature maps
-        # and then the images
-        sampled_image_levels = list(zip(*sampled_image_levels))
-        sampled_image_levels = cat([cat(l, dim=0) for l in sampled_image_levels], dim=0)
-        permute_inds = cat([nonzero(sampled_image_levels == img_idx)[0]
-                for img_idx in range(num_images)], dim=0)
-
-        self._permute_inds = permute_inds
-
-        return anchors
-
-    def prepare_targets(self, anchors, targets):
-        """
-        This reimplents parts of the functionality of TargetPreparator.__call__
-        The reason being that we don't need bbox regression targets for
-        masks, so I decided to specialize it here instead of modifying
-        TargetPreparator. It might be worth considering modifying this once
-        I implement keypoints
-        """
-        # flip anchor representation to be first images, and then feature maps
-        anchors = list(zip(*anchors))
-        anchors = [cat_bbox(anchor) for anchor in anchors]
-
-        target_preparator = self.target_preparator
-        # TODO assert / resize anchors to have the same .size as targets?
-        matched_targets = target_preparator.match_targets_to_anchors(anchors, targets)
-        labels = []
-        keypoints = []
-        for matched_targets_per_image, anchors_per_image in zip(
-                matched_targets, anchors):
-            labels_per_image, keypoints_per_image = target_preparator.prepare_labels(
-                    matched_targets_per_image, anchors_per_image)
-            labels.append(labels_per_image)
-            keypoints.append(keypoints_per_image)
-        return labels, keypoints
-
-    def get_permutation_inds(self, anchors):
-        # anchors is in features - images order get the permutation to make it in
-        # image - features order
-        num_images = len(anchors[0])
-        # flatten anchors into a single list
-        flattened = [f for l in anchors for f in l]
-        sizes = [i.bbox.shape[0] for i in flattened]
-        # device = torch.device('cpu')
-        device = flattened[0].bbox.device
-        # strategy: start from the identity permutation which has the final size
-        # split it according to the sizes for each feature map / image, group the
-        # indices according to a list of features of list of images, invert the
-        # representation to be images -> features, and then concatenate it all
-        inds = torch.arange(sum(sizes), device=device)
-        # can't use torch.split because of a bug with 0 in sizes
-        inds = split_with_sizes(inds, sizes)
-        # grouped correspond to the linear indices split in
-        # features first, and then images
-        grouped = [inds[i:i+num_images] for i in range(0, len(inds), num_images)]
-        # convert to images first, then features by flipping the representation
-        flip = list(zip(*grouped))
-        # flatten the list of lists into a single list of tensors
-        flip = [f for l in flip for f in l]
-        return torch.cat(flip, dim=0)
-
-    def __call__(self, anchors, keypoint_logits):
-        # labels, keypoint_targets = self.prepare_targets(anchors, targets)
-
-        # labels = self._labels
-        # keypoints = self._keypoints
-        # sampled_pos_inds = torch.cat(self._sampled_pos_inds, dim=0)
-        # permute_inds = self._permute_inds
-
-        permutation_inds = self.get_permutation_inds(anchors)
-        # from IPython import embed; embed()
-        # TODO is this required???
-        # mask_logits = cat(mask_logits, dim=0)
-        keypoint_logits = keypoint_logits[permutation_inds]
-        
-
-        anchors = list(zip(*anchors))
-        # anchors = [cat_bbox(anchor) for anchor in anchors]
-        keypoints = []
-        for anchors_per_image in anchors:
-            kp = [anchor.get_field('keypoints').keypoints for anchor in anchors_per_image]
-            kp = cat(kp, 0)
-            keypoints.append(kp)
-        # keypoints = [anchor.get_field('keypoints') for anchor in anchors]
-        new_anchors = []
-        for anchor in anchors:
-            new_anchors.append([a.copy_with_fields([]) for a in anchor])
-        # TODO make cat_bbox work with Keypoint
-        anchors = [cat_bbox(anchor) for anchor in new_anchors]
+    def __call__(self, proposals, keypoint_logits):
         heatmaps = []
         valid = []
-        for kp, anchor in zip(keypoints, anchors):
-            heatmaps_per_image, valid_per_image = self.target_preparator.project(kp, anchor)
+        for proposals_per_image in proposals:
+            kp = proposals_per_image.get_field("keypoints")
+            heatmaps_per_image, valid_per_image = project_keypoints_to_heatmap(kp, proposals_per_image, self.discretization_size)
             heatmaps.append(heatmaps_per_image.view(-1))
             valid.append(valid_per_image.view(-1))
+
         keypoint_targets = cat(heatmaps, dim=0)
-        valid = cat(valid, dim=0).to(dtype=torch.uint8)# .bool()
+        valid = cat(valid, dim=0).to(dtype=torch.uint8)
         valid = torch.nonzero(valid).squeeze(1)
-        # labels = cat(labels, dim=0)
-        # keypoint_targets = cat(targets, dim=0)
+
 
         MIN_KEYPOINT_COUNT_FOR_VALID_MINIBATCH = 20
-        num_valid = valid.sum().item()
+        num_valid = len(valid)
         # torch.mean (in binary_cross_entropy_with_logits) does'nt
         # accept empty tensors, so handle it sepaartely
         if keypoint_targets.numel() == 0 or valid.numel() == 0 or num_valid <= MIN_KEYPOINT_COUNT_FOR_VALID_MINIBATCH:
+            global _TOTAL_SKIPPED
+            _TOTAL_SKIPPED += 1
+            print("Non valid, skipping {}", _TOTAL_SKIPPED)
             return keypoint_logits.sum() * 0
 
         N, K, H, W = keypoint_logits.shape
@@ -251,5 +179,14 @@ class KeypointRCNNLossComputation(object):
 
 
 def make_roi_keypoint_loss_evaluator(cfg):
-    target_preparator = KeypointTargetPreparator(matcher, resolution)
-    loss_evaluator = KeypointRCNNLossComputation(target_preparator, fg_bg_sampler)
+    matcher = Matcher(
+        cfg.MODEL.ROI_HEADS.FG_IOU_THRESHOLD,
+        cfg.MODEL.ROI_HEADS.BG_IOU_THRESHOLD,
+        allow_low_quality_matches=False,
+    )
+    fg_bg_sampler = BalancedPositiveNegativeSampler(
+        cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE, cfg.MODEL.ROI_HEADS.POSITIVE_FRACTION
+    )
+    resolution = cfg.MODEL.ROI_KEYPOINT_HEAD.RESOLUTION
+    loss_evaluator = KeypointRCNNLossComputation(matcher, fg_bg_sampler, resolution)
+    return loss_evaluator
