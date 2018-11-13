8d5455c4ea99a87e0b9e40465ee77f77d50b50c6
Thomas Viehmann <tv@beamnet.de>
2018-11-13 12:48:35 +0100

    Cleanup: nms
    With recent JIT improvements, we can use the op directly

--- maskrcnn_benchmark/layers/nms.py
+++ maskrcnn_benchmark/layers/nms.py
@@ -1,22 +1,10 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
-# from ._utils import _C
 import torch
 
 # we need this for the custom ops to exist
 import maskrcnn_benchmark._custom_ops   # noqa: F401
 
-if 0:  # the easy way
-    nms = torch.ops.maskrcnn_benchmark.nms
+nms = torch.ops.maskrcnn_benchmark.nms
 
-
-def nms(dets, scores, threshold):
-    """This function performs Non-maximum suppresion"""
-    @torch.jit.script
-    def _nms1(dets, scores, threshold: float=threshold):
-        return torch.ops.maskrcnn_benchmark.nms(dets, scores, threshold)
-
-    @torch.jit.script
-    def _nms(dets, scores):
-        return _nms1(dets, scores)
-
-    return _nms(dets, scores)
+# nms.__doc__ = """
+# This function performs Non-maximum suppresion"""
