a7fb74f744d208efc6fc9170b9e5a33b101cb27d
keineahnung2345 <mimifasosofamire1123@gmail.com>
2018-11-23 20:07:33 +0800

    clean up

--- docker/docker-jupyter/jupyter_notebook_config.py
+++ docker/docker-jupyter/jupyter_notebook_config.py
@@ -17,7 +17,7 @@ from IPython.lib import passwd
 
 #c = c  # pylint:disable=undefined-variable
 c = get_config()
-c.NotebookApp.ip = '0.0.0.0' #c.NotebookApp.ip = '*'
+c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = int(os.getenv('PORT', 8888))
 c.NotebookApp.open_browser = False
 
