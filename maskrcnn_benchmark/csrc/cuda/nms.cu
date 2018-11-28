d48bfd7320b0e1a6a8b5197eb98f3cfdb1cb45c6
Yuxin Wu <ppwwyyxxc@gmail.com>
2018-11-28 13:05:21 -0800

    Fix indexing error in nms kernel
    Here it is indexing a cuda tensor with CPU indices. It used to work, but 
    after https://github.com/pytorch/pytorch/commit/006505bb8f9dcf0f38b32518308d071c8a1ccec6 it results in memory corruption.

--- maskrcnn_benchmark/csrc/cuda/nms.cu
+++ maskrcnn_benchmark/csrc/cuda/nms.cu
@@ -124,5 +124,5 @@ at::Tensor nms_cuda(const at::Tensor boxes, float nms_overlap_thresh) {
 
   THCudaFree(state, mask_dev);
   // TODO improve this part
-  return std::get<0>(order_t.index({keep.narrow(/*dim=*/0, /*start=*/0, /*length=*/num_to_keep)}).sort(0, false));
+  return std::get<0>(order_t.index({keep.narrow(/*dim=*/0, /*start=*/0, /*length=*/num_to_keep).cuda()}).sort(0, false));
 }
