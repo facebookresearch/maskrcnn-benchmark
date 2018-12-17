548d6ad249eb44eddf417344e579c990eba97e6e
Francisco Massa <fvsmassa@gmail.com>
2018-12-17 02:33:58 -0800

    Fix error in MetricLogger for non-existing attributes

--- maskrcnn_benchmark/utils/metric_logger.py
+++ maskrcnn_benchmark/utils/metric_logger.py
@@ -52,7 +52,10 @@ class MetricLogger(object):
     def __getattr__(self, attr):
         if attr in self.meters:
             return self.meters[attr]
-        return object.__getattr__(self, attr)
+        if attr in self.__dict__:
+            return self.__dict__[attr]
+        raise AttributeError("'{}' object has no attribute '{}'".format(
+                    type(self).__name__, attr))
 
     def __str__(self):
         loss_str = []
