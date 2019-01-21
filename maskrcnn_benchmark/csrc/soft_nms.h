6d4823786b47500ac586cfc62afdce8eb17c4cd9
lyakaap <bepemgdlp@gmail.com>
2019-01-21 18:02:27 +0900

    Create soft_nms.h

---
+++ maskrcnn_benchmark/csrc/soft_nms.h
@@ -0,0 +1,24 @@
+// Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+#pragma once
+#include "cpu/vision.h"
+
+#ifdef WITH_CUDA
+#include "cuda/vision.h"
+#endif
+
+
+at::Tensor soft_nms(const at::Tensor& dets,
+                    at::Tensor& scores,
+                    const float threshold = 0.0001,
+                    const float sigma = 0.5) {
+
+  if (dets.type().is_cuda()) {
+#ifdef WITH_CUDA
+    AT_ERROR("Soft NMS Does Not have GPU support");
+#endif
+  }
+
+  at::Tensor result = soft_nms_cpu(dets, scores, threshold, sigma);
+
+  return result;
+}
