d1e74a1e540999cd313c9d65395e6da1ba8da4d9
lyakaap <bepemgdlp@gmail.com>
2019-01-21 18:04:51 +0900

    Create soft_nms.py

---
+++ maskrcnn_benchmark/layers/soft_nms.py
@@ -0,0 +1,8 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+# from ._utils import _C
+from maskrcnn_benchmark import _C
+
+soft_nms = _C.soft_nms
+
+# nms.__doc__ = """
+# This function performs Non-maximum suppresion"""
