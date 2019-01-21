9945135676719570c22cdf3709af73dbd132ea61
lyakaap <bepemgdlp@gmail.com>
2019-01-21 18:03:29 +0900

    Update vision.cpp

--- maskrcnn_benchmark/csrc/vision.cpp
+++ maskrcnn_benchmark/csrc/vision.cpp
@@ -2,9 +2,11 @@
 #include "nms.h"
 #include "ROIAlign.h"
 #include "ROIPool.h"
+#include "soft_nms.h"
 
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+  m.def("soft_nms", &soft_nms, "soft-non-maximum suppression");
   m.def("nms", &nms, "non-maximum suppression");
   m.def("roi_align_forward", &ROIAlign_forward, "ROIAlign_forward");
   m.def("roi_align_backward", &ROIAlign_backward, "ROIAlign_backward");
