58da4d58dcb506aa867ee3f7b4d9736b3463f819
dongdk <dongdk.sysu@foxmail.com>
2019-01-21 11:00:26 +0800

    modification on yaml files in configs/gn_baselines/ and reduce the amount of indentation and code duplication

--- maskrcnn_benchmark/modeling/make_layers.py
+++ maskrcnn_benchmark/modeling/make_layers.py
@@ -67,19 +67,14 @@ def make_conv3x3(
         torch.nn.init.normal_(conv.weight, std=0.01)
     if not use_gn:
         nn.init.constant_(conv.bias, 0)
+    module = [conv,]
     if use_gn:
-        if use_relu:
-            return nn.Sequential(
-                conv, group_norm(out_channels), nn.ReLU(inplace=True)
-            )
-        else:
-            return nn.Sequential(
-                conv, group_norm(out_channels)
-            )
+        module.append(group_norm(out_channels))
     if use_relu:
-        return nn.Sequential(conv, nn.ReLU(inplace=True))
-    else:
-        return conv
+        module.append(nn.ReLU(inplace=True))
+    if len(module) > 1:
+        return nn.Sequential(*module)
+    return conv
 
 
 def make_fc(dim_in, hidden_dim, use_gn):
@@ -101,7 +96,7 @@ def conv_with_kaiming_uniform(use_gn=False, use_relu=False):
     def make_conv(
         in_channels, out_channels, kernel_size, stride=1, dilation=1
     ):
-        module = Conv2d(
+        conv = Conv2d(
             in_channels, 
             out_channels, 
             kernel_size=kernel_size, 
@@ -112,20 +107,16 @@ def conv_with_kaiming_uniform(use_gn=False, use_relu=False):
         )
         # Caffe2 implementation uses XavierFill, which in fact
         # corresponds to kaiming_uniform_ in PyTorch
-        nn.init.kaiming_uniform_(module.weight, a=1)
+        nn.init.kaiming_uniform_(conv.weight, a=1)
         if not use_gn:
-            nn.init.constant_(module.bias, 0)
+            nn.init.constant_(conv.bias, 0)
+        module = [conv,]
         if use_gn:
-            if use_relu:
-                return nn.Sequential(
-                    module, group_norm(out_channels), nn.ReLU(inplace=True)
-                )
-            else:
-                return nn.Sequential(
-                    module, group_norm(out_channels)
-                )
+            module.append(group_norm(out_channels))
         if use_relu:
-            return nn.Sequential(module, nn.ReLU(inplace=True))
-        else:
-            return module
+            module.append(nn.ReLU(inplace=True))
+        if len(module) > 1:
+            return nn.Sequential(*module)
+        return conv
+
     return make_conv
