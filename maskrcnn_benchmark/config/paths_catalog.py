1cd78d10399ebc5ad66c7be5d05cda2ff9ba729a
Cheng-Yang Fu <cyfu@cs.unc.edu>
2019-01-23 07:20:03 -0500

    Add COCO_2017 in paths_catalog.py (#373)
    * Add new section "Projects using maskrcnn-benchmark".
    
    * Update README.md
    
    update the format.
    
    * Update README.md
    
    * Add coco_2017_train and coco_2017_val
    
    * Update README.md
    
    Add the instructions about COCO_2017

--- maskrcnn_benchmark/config/paths_catalog.py
+++ maskrcnn_benchmark/config/paths_catalog.py
@@ -7,6 +7,14 @@ import os
 class DatasetCatalog(object):
     DATA_DIR = "datasets"
     DATASETS = {
+        "coco_2017_train": {
+            "img_dir": "coco/train2017",
+            "ann_file": "coco/annotations/instances_train2017.json"
+        },
+        "coco_2017_val": {
+            "img_dir": "coco/val2017",
+            "ann_file": "coco/annotations/instances_val2017.json"
+        },
         "coco_2014_train": {
             "img_dir": "coco/train2014",
             "ann_file": "coco/annotations/instances_train2014.json"
