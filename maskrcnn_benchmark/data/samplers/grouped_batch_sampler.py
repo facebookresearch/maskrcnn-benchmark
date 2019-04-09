6566ac55f940d0885a1847d694d9fb6bc0628677
WinticsAI <levi.viana@wintics.com>
2019-04-09 22:48:29 +0200

    Fixing IndexError on grouped_batch_sampler.py

--- maskrcnn_benchmark/data/samplers/grouped_batch_sampler.py
+++ maskrcnn_benchmark/data/samplers/grouped_batch_sampler.py
@@ -76,7 +76,7 @@ class GroupedBatchSampler(BatchSampler):
         # the order that we have in the sampler. For that, we will consider the
         # ordering as coming from the first element of each batch, and sort
         # correspondingly
-        first_element_of_batch = [t[0].item() for t in merged]
+        first_element_of_batch = [t[0].item() for t in merged if t.numel() > 0]
         # get and inverse mapping from sampled indices and the position where
         # they occur (as returned by the sampler)
         inv_sampled_ids_map = {v: k for k, v in enumerate(sampled_ids.tolist())}
