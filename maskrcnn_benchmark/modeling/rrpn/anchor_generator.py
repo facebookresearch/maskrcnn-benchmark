updated rrpn regression targets normalization, added make_width_larger to anchor_generator.py, fixed visualization bug in infer_demo.py, added new test_rpn_loss file

maskrcnn_benchmark/modeling/rrpn/anchor_generator.py
@@ -17,7 +17,8 @@ class AnchorGenerator(torch.nn.Module):
             aspect_ratios=(0.5, 1.0, 2.0),
             anchor_strides=(16),
             anchor_angles=(-90),
-            straddle_thresh=0
+            straddle_thresh=0,
+            make_width_larger=True
     ):
         super(AnchorGenerator, self).__init__()
         sizes = anchor_sizes
@@ -45,6 +46,8 @@ class AnchorGenerator(torch.nn.Module):
         # self.cell_anchors = BufferList(cell_anchors)
         self.straddle_thresh = straddle_thresh
 
+        self.make_width_larger = make_width_larger
+
     def num_anchors_per_location(self):
         return [len(sizes) * len(self.aspect_ratios) * len(self.anchor_angles) for sizes in self.anchor_sizes]
 
@@ -55,7 +58,8 @@ class AnchorGenerator(torch.nn.Module):
         ):
             h = grid_size[0]  # * stride
             w = grid_size[1]  # * stride
-            anchor = generate_anchors(sizes, self.aspect_ratios, self.anchor_angles, h, w, stride)
+            anchor = generate_anchors(sizes, self.aspect_ratios, self.anchor_angles,
+                                      h, w, stride, make_width_larger=self.make_width_larger)
 
             anchors.append(torch.tensor(anchor, dtype=torch.float32, device=device))
             # anchors.append(anchor)
@@ -114,6 +118,7 @@ def make_anchor_generator(config):
     anchor_angles = CFG_RPN.ANCHOR_ANGLES
     straddle_thresh = CFG_RPN.STRADDLE_THRESH
 
+    assert len(anchor_angles) > 0, "Anchor angles cannot be empty"
     if CFG_RPN.USE_FPN:
         # raise NotImplementedError
         assert len(anchor_stride) == len(
@@ -121,8 +126,14 @@ def make_anchor_generator(config):
         ), "FPN should have len(ANCHOR_STRIDE) == len(ANCHOR_SIZES)"
     else:
         assert len(anchor_stride) == 1, "Non-FPN should have a single ANCHOR_STRIDE"
+
+    # check if goes beyond 90
+    min_angle = np.min(anchor_angles)
+    max_angle = np.max(anchor_angles)
+    make_width_larger = np.abs(max_angle - min_angle) > 90
     anchor_generator = AnchorGenerator(
-        anchor_sizes, aspect_ratios, anchor_stride, anchor_angles, straddle_thresh
+        anchor_sizes, aspect_ratios, anchor_stride,
+        anchor_angles, straddle_thresh, make_width_larger
     )
     return anchor_generator
 
@@ -171,7 +182,7 @@ def enum_ratios_and_thetas2(anchors, anchor_ratios, anchor_angles):
 # def generate_anchors(base_anchor_size, anchor_scales, anchor_ratios, anchor_angles,
 #                  height, width, stride):
 def generate_anchors(anchor_sizes, anchor_ratios, anchor_angles,
-                     height, width, stride):
+                     height, width, stride, make_width_larger=True):
     """
     returns anchors: (N, 5), each element is [x_center,y_center,width,height,angle]
     N = H * W * (len anchor_sizes * len anchor_ratios * len anchor_angles)
@@ -202,26 +213,28 @@ def generate_anchors(anchor_sizes, anchor_ratios, anchor_angles,
     box_parameters = np.reshape(box_parameters, [-1, 3])
     anchors = np.concatenate([anchor_centers, box_parameters], axis=1)
 
-    h_gt_w = anchors[:, 3] >= anchors[:, 2]
-    anchors[h_gt_w, 2:4] = anchors[h_gt_w, 2:4][:, :-1]  # always make width bigger than height
-    anchors[h_gt_w, -1] -= 90
-    anchors[anchors[:, -1] < -90, -1] += 180
-    anchors[anchors[:, -1] > 90, -1] -= 180
+    if make_width_larger:
+        h_gt_w = anchors[:, 3] >= anchors[:, 2]
+        anchors[h_gt_w, 2:4] = anchors[h_gt_w, 2:4][:, :-1]  # always make width bigger than height
+        anchors[h_gt_w, -1] -= 90
+        anchors[anchors[:, -1] < -90, -1] += 180
+        anchors[anchors[:, -1] > 90, -1] -= 180
 
     return anchors
 
 
-def convert_pts_to_rect(pts):
+def convert_pts_to_rect(pts, make_width_larger=True):
     rect1 = cv2.minAreaRect(pts)
 
     x, y, w, h, theta = rect1[0][0], rect1[0][1], rect1[1][0], rect1[1][1], rect1[2]
-    if h >= w:
-        h, w = w, h
-        theta = theta - 90
-    if theta < -90.0:
-        theta = theta + 180
-    elif theta > 90.0:
-        theta = theta - 180
+    if make_width_larger:
+        if h >= w:
+            h, w = w, h
+            theta = theta - 90
+        if theta < -90.0:
+            theta = theta + 180
+        elif theta > 90.0:
+            theta = theta - 180
     return (x, y, w, h, theta)
 
 
@@ -299,7 +312,7 @@ def draw_anchors(img, anchors, color_list=[], fill=False, line_sz=2):
     for ix, anchor in enumerate(anchors):
         color = color_list[ix]
         rect = anchor
-        if len(anchor) != 8:
+        if len(anchor) == 5:
             rect = convert_rect_to_pts(anchor)
         rect = np.round(rect).astype(np.int32)
         if fill:

maskrcnn_benchmark/modeling/rrpn/loss.py
@@ -20,6 +20,26 @@ from maskrcnn_benchmark.modeling.rrpn.utils import \
     get_boxlist_rotated_rect_tensor, concat_box_prediction_layers
 
 
+def normalize_reg_targets(reg_targets):
+    dims = reg_targets.shape
+    assert len(dims) == 2 and dims[1] == 5
+
+    reg_angles = reg_targets[:, -1]
+    reg_angles_sign = (reg_angles > 0).to(torch.float32)
+    reg_angles_sign[reg_angles_sign == 0] = -1
+    reg_angles_abs = torch.abs(reg_angles)
+    gt_45 = reg_angles_abs > 0.7854 # np.deg2rad(45)
+    gt_135 = reg_angles_abs > 2.3561 # np.deg2rad(135)
+    lt_135 = ~gt_135 # np.deg2rad(135)
+    gt_45_lt_135 = gt_45 * lt_135
+
+    reg_targets[gt_45_lt_135, -1] -= reg_angles_sign[gt_45_lt_135] * 1.5708 # np.deg2rad(90)
+    xd = reg_targets[gt_45_lt_135, 2:4]
+    reg_targets[gt_45_lt_135, 2] = xd[:, 1]
+    reg_targets[gt_45_lt_135, 3] = xd[:, 0]
+    reg_targets[gt_135, -1] -= reg_angles_sign[gt_135] * 3.1416 # np.deg2rad(180)
+
+
 class RPNLossComputation(object):
     """
     This class computes the RPN loss.
@@ -56,11 +76,6 @@ class RPNLossComputation(object):
         target_tensor = get_boxlist_rotated_rect_tensor(target, masks_field, rrects_field)
 
         match_quality_matrix = rotate_iou(target_tensor, anchor_tensor)
-
-        # CUSTOM LOGIC: SET ALL ANCHORS VS TARGETS WITH ROTATION DIFF > angle threshold TO IOU OF 0 (to prevent rotation ambiguity)
-        ANGLE_THRESH = 45 # degrees
-        angle_diffs = torch.abs(target_tensor[:,-1].unsqueeze(-1) - anchor_tensor[:, -1])
-        match_quality_matrix[angle_diffs >= ANGLE_THRESH] = 0
 
         matched_idxs = self.proposal_matcher(match_quality_matrix)
         # RPN doesn't need any fields from target
@@ -100,9 +115,11 @@ class RPNLossComputation(object):
                 inds_to_discard = matched_idxs == Matcher.BETWEEN_THRESHOLDS
                 labels_per_image[inds_to_discard] = -1
 
+            target_rrects = matched_targets.get_field("rrects")
+            anchor_rrects = anchors_per_image.get_field("rrects")
             # compute regression targets
             regression_targets_per_image = self.box_coder.encode(
-                matched_targets.get_field("rrects"), anchors_per_image.get_field("rrects")
+                target_rrects, anchor_rrects
             )
 
             labels.append(labels_per_image)
@@ -146,15 +163,16 @@ class RPNLossComputation(object):
 
         pos_regression = box_regression[sampled_pos_inds]
         pos_regression_targets = regression_targets[sampled_pos_inds]
+        normalize_reg_targets(pos_regression_targets)
         box_loss = smooth_l1_loss(
-            pos_regression[:, :-1],
-            pos_regression_targets[:, :-1],
+            pos_regression,#[:, :-1],
+            pos_regression_targets,#[:, :-1],
             beta=1.0 / 9,
             size_average=False,
         )
         box_loss = box_loss / total_pos
 
-        angle_loss = torch.abs(torch.sin(pos_regression[:, -1] - pos_regression_targets[:, -1])).mean()
+        angle_loss = 0 # 0.5 * torch.abs(torch.sin(pos_regression[:, -1] - pos_regression_targets[:, -1])).mean()
 
         # balance negative and positive weights
         sampled_labels = labels[sampled_inds]

my_tools/infer_demo.py
@@ -7,6 +7,9 @@ from torchvision.transforms import functional as TVF
 import numpy as np
 from transforms3d.quaternions import quat2mat#, mat2quat
 
+import os
+import os.path as osp
+
 import torch
 import torch.nn.functional as F
 
@@ -323,12 +326,15 @@ if __name__ == '__main__':
 
     CLASSES = ["__background__", "class_x"]
     # config_file = "./configs/rebin.yaml"
-    config_file = "./configs/loading_bbox.yaml"
+    config_file = "./configs/coco_rpn_only.yaml"
 
-    model_file = "./checkpoints/loading_bbox_rotated_rpn_only/model_final.pth"
-    image_dir = "/home/bot/LabelMe/Images/loading_test"
+    model_file = "./checkpoints/coco_rotated_rpn_only/model_final.pth"
+    # model_file = "./checkpoints/coco_rpn_only/model_final.pth"
+    image_dir = "datasets/coco/val2014"
     # image_files = ["mixed/temple_0/000885.left","mixed/temple_0/001774.left"]
     image_ext = ".jpg"
+    image_files = ["COCO_val2014_000000001000.jpg", "COCO_val2014_000000010012.jpg"]
+    image_files = [osp.join(image_dir, f) for f in image_files]
 
     cfg.merge_from_file(config_file)
     img_transformer = ImageTransformer(cfg)
@@ -356,7 +362,7 @@ if __name__ == '__main__':
 
     max_depth = 7
 
-    for image_file in glob.glob("%s/*%s"%(image_dir, image_ext)):
+    for image_file in image_files: #glob.glob("%s/*%s"%(image_dir, image_ext)):
     # for image_file in glob.glob("%s/*1-%s"%(image_dir, image_ext)):
     # for image_file in ["%s/%s%s"%(image_dir, f, image_ext) for f in image_files]:
         img = cv2.imread(image_file)
@@ -422,7 +428,7 @@ if __name__ == '__main__':
                 w_ratio = float(width) / pred_size[0]
                 h_ratio = float(height) / pred_size[1]
                 rrects = predictions.get_field("rrects")
-                rr = rrects.cpu().numpy()
+                rr = rrects.cpu().numpy().copy()
                 rr[:, 0] *= w_ratio
                 rr[:, 2] *= w_ratio
                 rr[:, 1] *= h_ratio