501e5d0ca27f960b8ba63904a13f886cff5eafff
mrlooi <user.name>
2019-04-26 10:16:10 +0800

    remove posecnn related csrc from master

--- maskrcnn_benchmark/csrc_custom/cuda/ave_dist_loss_cuda.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <torch/extension.h>
-
-std::vector<at::Tensor> ave_dist_loss_forward_cuda(
-    const at::Tensor& poses_pred, const at::Tensor& poses_target, const at::Tensor& poses_labels, const at::Tensor& points, const at::Tensor& symmetry,
-    const float margin);
-
-at::Tensor ave_dist_loss_backward_cuda(const at::Tensor& grad, const at::Tensor& bottom_diff);
