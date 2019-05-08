482ef32264892190c9a588204533bdee91170852
ugriffo <umberto.griffo@gmail.com>
2019-05-08 14:04:53 +0200

    Tracking multiple objects across video frames demo

--- test.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-python tools/test_net.py --config-file "configs/potato_mask_rcnn_R_50_FPN_1x.yaml" TEST.IMS_PER_BATCH 4
--- train.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-python tools/train_net.py --config-file "configs/potato_mask_rcnn_R_50_FPN_1x.yaml" SOLVER.IMS_PER_BATCH 2
