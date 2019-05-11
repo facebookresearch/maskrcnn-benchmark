3f5f4e514d91526d46f6b6bf6ebd9d9ac6e92d9b
Ouail <ouail.bendidi@gmail.com>
2019-05-11 22:50:12 +0200

    Update trainer.py

--- maskrcnn_benchmark/engine/trainer.py
+++ maskrcnn_benchmark/engine/trainer.py
@@ -54,7 +54,7 @@ def do_train(
     start_training_time = time.time()
     end = time.time()
     for iteration, (images, targets, _) in enumerate(data_loader, start_iter):
-        print("what pairs of images id that I am using", _ )
+        print(f"Iteration={iteration + 1} || Image Ids used for training {_} || images shape={images.shape}" )
         data_time = time.time() - end
         iteration = iteration + 1
         arguments["iteration"] = iteration
