910b7385a62d7bba7b3faa2145ef6ca67152c6d0
vincentlv <looicj@msn.com>
2019-05-18 21:53:20 +0800

    added apex back to all relevant files

--- tools/test_net.py
+++ tools/test_net.py
@@ -17,11 +17,11 @@ from maskrcnn_benchmark.utils.comm import synchronize, get_rank
 from maskrcnn_benchmark.utils.logger import setup_logger
 from maskrcnn_benchmark.utils.miscellaneous import mkdir
 
-# # Check if we can enable mixed-precision via apex.amp
-# try:
-#     from apex import amp
-# except ImportError:
-#     raise ImportError('Use APEX for mixed precision via apex.amp')
+# Check if we can enable mixed-precision via apex.amp
+try:
+    from apex import amp
+except ImportError:
+    raise ImportError('Use APEX for mixed precision via apex.amp')
 
 
 def main():
@@ -69,7 +69,7 @@ def main():
 
     # Initialize mixed-precision if necessary
     use_mixed_precision = cfg.DTYPE == 'float16'
-    # amp_handle = amp.init(enabled=use_mixed_precision, verbose=cfg.AMP_VERBOSE)
+    amp_handle = amp.init(enabled=use_mixed_precision, verbose=cfg.AMP_VERBOSE)
 
     output_dir = cfg.OUTPUT_DIR
     checkpointer = DetectronCheckpointer(cfg, model, save_dir=output_dir)
