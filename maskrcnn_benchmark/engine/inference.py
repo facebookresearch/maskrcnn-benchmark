33664fc00066d1568dad28128407cfb615092e4f
Hongliang Yuan <fallingdust@gmail.com>
2019-05-21 19:59:30 +0800

    Fix missing cfg after merge.

--- maskrcnn_benchmark/engine/inference.py
+++ maskrcnn_benchmark/engine/inference.py
@@ -6,6 +6,7 @@ import os
 import torch
 from tqdm import tqdm
 
+from maskrcnn_benchmark.config import cfg
 from maskrcnn_benchmark.data.datasets.evaluation import evaluate
 from ..utils.comm import is_main_process, get_world_size
 from ..utils.comm import all_gather
