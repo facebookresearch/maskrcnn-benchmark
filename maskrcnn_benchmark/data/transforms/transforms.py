4c6cd1a4bb55c656cd853ca6b41691587a72ee2d
hcx1231 <32597852+hcx1231@users.noreply.github.com>
2019-05-29 03:56:36 +0800

    add vertical flip (#818)
    * keep the resize function the same in test time the same with training time
    
    * add vertical flip

--- maskrcnn_benchmark/data/transforms/transforms.py
+++ maskrcnn_benchmark/data/transforms/transforms.py
@@ -73,6 +73,15 @@ class RandomHorizontalFlip(object):
             target = target.transpose(0)
         return image, target
 
+class RandomVerticalFlip(object):
+    def __init__(self, prob=0.5):
+        self.prob = prob
+
+    def __call__(self, image, target):
+        if random.random() < self.prob:
+            image = F.vflip(image)
+            target = target.transpose(1)
+        return image, target
 
 class ColorJitter(object):
     def __init__(self,
