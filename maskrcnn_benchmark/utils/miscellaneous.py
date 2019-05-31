e3ea62d4f0581a560be4daefe676ca8013220d85
miguel <miguel.varelaramos@braincreators.com>
2019-05-31 11:10:14 +0200

    save labels on main process only

--- maskrcnn_benchmark/utils/miscellaneous.py
+++ maskrcnn_benchmark/utils/miscellaneous.py
@@ -3,6 +3,7 @@ import errno
 import json
 import logging
 import os
+from .comm import is_main_process
 
 
 def mkdir(path):
@@ -14,18 +15,19 @@ def mkdir(path):
 
 
 def save_labels(dataset_list, output_dir):
-    logger = logging.getLogger(__name__)
+    if is_main_process():
+        logger = logging.getLogger(__name__)
 
-    ids_to_labels = {}
-    for dataset in dataset_list:
-        if hasattr(dataset, 'categories'):
-            ids_to_labels.update(dataset.categories)
-        else:
-            logger.warning("Dataset [{}] has no categories attribute, labels.json file won't be created".format(
-                dataset.__class__.__name__))
+        ids_to_labels = {}
+        for dataset in dataset_list:
+            if hasattr(dataset, 'categories'):
+                ids_to_labels.update(dataset.categories)
+            else:
+                logger.warning("Dataset [{}] has no categories attribute, labels.json file won't be created".format(
+                    dataset.__class__.__name__))
 
-    if ids_to_labels:
-        labels_file = os.path.join(output_dir, 'labels.json')
-        logger.info("Saving labels mapping into {}".format(labels_file))
-        with open(labels_file, 'w') as f:
-            json.dump(ids_to_labels, f, indent=2)
+        if ids_to_labels:
+            labels_file = os.path.join(output_dir, 'labels.json')
+            logger.info("Saving labels mapping into {}".format(labels_file))
+            with open(labels_file, 'w') as f:
+                json.dump(ids_to_labels, f, indent=2)
