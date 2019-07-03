f670a83ead9d2ddf8ecb2c7093e501e5574430b6
vinace <vinsent.storage@gmail.com>
2019-07-03 18:13:27 +0530

    checking for valid annotation ids in the dataset formation

--- maskrcnn_benchmark/data/datasets/micr.py
+++ maskrcnn_benchmark/data/datasets/micr.py
@@ -43,7 +43,7 @@ class MICRDataset(torchvision.datasets.coco.CocoDetection):
     def __init__(
         self, ann_file, root, remove_images_without_annotations, transforms=None
     ):
-        super(MICRDataset, self).__init__(root, ann_file)
+        super().__init__(self, root, ann_file)
         # sort indices for reproducible results
         self.ids = sorted(self.ids)
         print(self.ids)
