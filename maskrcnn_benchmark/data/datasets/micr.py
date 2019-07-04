d48759c87d571e742eed5641d60c11232f04ff93
vinace <vinsent.storage@gmail.com>
2019-07-04 17:24:04 +0530

    checking for valid annotation ids in the dataset formation

--- maskrcnn_benchmark/data/datasets/micr.py
+++ maskrcnn_benchmark/data/datasets/micr.py
@@ -81,7 +81,7 @@ class MICRDataset(torchvision.datasets.coco.CocoDetection):
 
     def __getitem__(self, idx):
         
-        img, anno = super(MICRDataset, self).__getitem__(idx)
+        img, anno = super(COCODataset, self).__getitem__(idx)
 
         # filter crowd annotations
         # TODO might be better to add an extra field
