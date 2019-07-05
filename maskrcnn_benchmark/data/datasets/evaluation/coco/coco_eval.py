8697f786339bbb25e3aad3185d7b228ae29b8db3
vinace <vinsent.storage@gmail.com>
2019-07-05 12:01:54 +0530

    checking the evaluation test set with the default imagenet weight

--- maskrcnn_benchmark/data/datasets/evaluation/coco/coco_eval.py
+++ maskrcnn_benchmark/data/datasets/evaluation/coco/coco_eval.py
@@ -70,6 +70,7 @@ def do_coco_evaluation(
 def prepare_for_coco_detection(predictions, dataset):
     # assert isinstance(dataset, COCODataset)
     coco_results = []
+    import pdb; pdb.set_trace()
     for image_id, prediction in enumerate(predictions):
         original_id = dataset.id_to_img_map[image_id]
         if len(prediction) == 0:
