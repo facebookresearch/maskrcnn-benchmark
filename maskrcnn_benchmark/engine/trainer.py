b09165725a6665725efb6a636d5ae3f230410c6e
vinace <vinsent.storage@gmail.com>
2019-07-09 19:56:24 +0530

    changin the checkpoint iteration value to save checkpoint

--- maskrcnn_benchmark/engine/trainer.py
+++ maskrcnn_benchmark/engine/trainer.py
@@ -112,6 +112,8 @@ def do_train(
         import pdb;pdb.set_trace()
         # if iteration % checkpoint_period == 0:
         if iteration % 10 == 0:
+            logger.info("saving checkpoints")
+            print("savinggggggg checkpoint")
             checkpointer.save("model_{:07d}".format(iteration), **arguments)
         if iteration == max_iter:
             checkpointer.save("model_final", **arguments)
