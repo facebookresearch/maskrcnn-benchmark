787ba0e5be4f56ca98034cbe1ab51b36494e8670
vinace <vinsent.storage@gmail.com>
2019-07-10 14:50:35 +0530

    checking anno, img in __getitem__

--- maskrcnn_benchmark/data/datasets/micr.py
+++ maskrcnn_benchmark/data/datasets/micr.py
@@ -100,7 +100,7 @@ class MICRDataset(torchvision.datasets.coco.CocoDetection):
         classes = [obj["category_id"] for obj in anno]
         classes = [self.json_category_id_to_contiguous_id[c] for c in classes]
         classes = torch.tensor(classes)
-        target.add_field("categories", classes)
+        target.add_field("labels", classes)
         # target.add_field("labels", classes)
         if anno and "segmentation" in anno[0]:
             masks = [obj["segmentation"] for obj in anno]
