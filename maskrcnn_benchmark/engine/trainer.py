ab6b7ef8ab02cdc1baf6da804fee3a1847dfe722
vinace <vinsent.storage@gmail.com>
2019-07-12 15:42:02 +0530

    chnaged to 5000 iteration for training

--- maskrcnn_benchmark/engine/trainer.py
+++ maskrcnn_benchmark/engine/trainer.py
@@ -109,7 +109,7 @@ def do_train(
                 )
             )
         # if iteration % checkpoint_period == 0:
-        if iteration % 100 == 0:
+        if iteration % 1000 == 0:
             logger.info("saving checkpoints")
             print("savinggggggg checkpoint")
             checkpointer.save("model_{:07d}".format(iteration), **arguments)
