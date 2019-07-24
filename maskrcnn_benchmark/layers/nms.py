b0420d802d47d21650624ccf6915f0e45c741279
Ouail <ouail.bendidi@gmail.com>
2019-07-24 12:11:59 +0200

    Update nms.py

--- maskrcnn_benchmark/layers/nms.py
+++ maskrcnn_benchmark/layers/nms.py
@@ -2,10 +2,13 @@
 # from ._utils import _C
 from maskrcnn_benchmark import _C
 
-from apex import amp
-
-# Only valid with fp32 inputs - give AMP the hint
-nms = amp.float_function(_C.nms)
+try:
+  from apex import amp
+  # Only valid with fp32 inputs - give AMP the hint
+  nms = amp.float_function(_C.nms)
+except Exception as e:
+  print("Couldn't load apex, because you are running on cpu probably, and couldn't detect cuda !")
+  nms = _C.nms
 
 # nms.__doc__ = """
 # This function performs Non-maximum suppresion"""
