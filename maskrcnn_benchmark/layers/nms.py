429834b3457c9fa5a1fe77c89a804b4a8532c15c
vincent <looicj@msn.com>
2019-07-25 23:50:20 +0800

    fixed nms.py soft_nms bug by adding apex amp

--- maskrcnn_benchmark/layers/nms.py
+++ maskrcnn_benchmark/layers/nms.py
@@ -6,6 +6,7 @@ from apex import amp
 
 # Only valid with fp32 inputs - give AMP the hint
 nms = amp.float_function(_C.nms)
+soft_nms_f = amp.float_function(_C.soft_nms)
 
 # nms.__doc__ = """
 # This function performs Non-maximum suppresion"""
@@ -14,6 +15,6 @@ def soft_nms(boxes, scores, nms_thresh=0.3, sigma=0.5, score_thresh=0.001, metho
     # method: 1) linear, 2) gaussian, else) original NMS
     boxes2 = boxes.clone()
     scores2 = scores.clone()
-    indices, keep = _C.soft_nms(boxes2, scores2, nms_thresh, sigma, score_thresh, method)
+    indices, keep = soft_nms_f(boxes2, scores2, nms_thresh, sigma, score_thresh, method)
     return indices, keep, scores2
 
