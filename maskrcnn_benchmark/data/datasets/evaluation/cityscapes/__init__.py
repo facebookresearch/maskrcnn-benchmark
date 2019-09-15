8020d8730246f02a7b65894ce06293bc3c8727f5
botcs <botos.official@gmail.com>
2019-09-16 01:57:55 +0200

    CityScapes evaluation

---
+++ maskrcnn_benchmark/data/datasets/evaluation/cityscapes/__init__.py
@@ -0,0 +1,21 @@
+from .cityscapes_eval import do_cityscapes_evaluation
+
+
+def abs_cityscapes_evaluation(
+    dataset,
+    predictions,
+    box_only,
+    output_folder,
+    iou_types,
+    expected_results,
+    expected_results_sigma_tol,
+):
+    return do_cityscapes_evaluation(
+        dataset=dataset,
+        predictions=predictions,
+        box_only=box_only,
+        output_folder=output_folder,
+        iou_types=iou_types,
+        expected_results=expected_results,
+        expected_results_sigma_tol=expected_results_sigma_tol,
+    )
